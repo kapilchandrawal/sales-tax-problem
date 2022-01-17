8ef3540fcad012047ccc237aac27eaafaffa16f6
kapilchandrawal <kapilchandrawal001@gmail.com>
2022-01-17 17:24:08 +0530

    Added calculation for taxes

--- sales_tax.rb
+++ sales_tax.rb
@@ -5,5 +5,20 @@ class Product
     @price = price
     @exempt = exempt
     @imported = imported
+    @tax = 0
+  end
+
+  # Calculate the different taxes :-
+  def calculate_tax
+    if @exempt == false && @imported == true
+      @tax = @price * 0.5
+    elsif @exempt == false && @imported == false
+      @tax = @price * 0.10
+    elsif @exempt == true && @imported == false
+      @tax = 0
+    end
   end
 end
+
+# Create Objects that will be products:-
+book = Product.new("Thor", 13.99, true, false)
