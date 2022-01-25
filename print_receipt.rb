cc8aba1a692538abc150c2a8a9e08e082ccb831b
kapilchandrawal <kapilchandrawal001@gmail.com>
2022-01-25 17:31:14 +0530

    Changed some tax rates

--- print_receipt.rb
+++ print_receipt.rb
@@ -11,15 +11,13 @@ class Receipt
     @price_total += item.price * item.qty
     @tax_total += item.tax
     @qty_total = item.qty
-    # @grand_total += (@price_total * @qty_total) + item.tax
   end
 
   def print_list
     @list.each do |product|
-      puts "#{product.qty} #{product.item_name}: #{(product.price + product.tax).round(2)}"
-    end 
-    # puts "Subtotal: #{@price_total}"
-    puts "Sales Taxes: #{@tax_total.round(2)}"
-    puts "Total: #{@price_total.round(2) + @tax_total.round(2)}"
+      puts "#{product.qty} #{product.item_name}: #{((product.price + product.tax)*20).round/20.0}"
+    end
+    puts "Sales Taxes: #{(@tax_total*20).round/20.0}"
+    puts "Total: #{(@price_total*20).round/20.0 + (@tax_total*20).round/20.0}"
   end
 end
--- sales_tax.rb
+++ sales_tax.rb
@@ -1,7 +1,3 @@
-require_relative("print_receipt.rb")
-
-EXEMPT = %w[chocolates, book, pills, chocolate]
-PRODUCTS_ARR = []
 
 class Product
   attr_accessor :qty, :item_name, :price, :exempt, :imported, :tax
@@ -13,8 +9,6 @@ class Product
     @imported = imported
     @tax = tax
   end
-
-  # Calculate the different taxes :-
   def calculate_tax
     @tax = 0
     if @exempt == false && @imported == true
@@ -29,45 +23,3 @@ class Product
     @sales_tax = @tax
   end
 end
-
-no = 0
-puts "Number of products?"
-no = gets.chomp.to_i
-while no > 0
-  data = gets.chomp
-  split_data = data.split
-  qty = split_data[0].to_i
-  price = split_data[-1].to_f
-  p = data.split(" at ")
-  product = p[0].delete("/0-9/").strip
-  # puts product
-  exempt = false
-  imported = false
-  if product.include?('imported')
-    imported = true
-  end
-  # puts product && $EXEMPT
-  chck_product = product.slice! "imported".strip
-  product = product.strip
-  EXEMPT.each do |px|
-    if px.eql?(product)
-      exempt = true
-      # puts product
-      break
-    end
-  end
-  
-  # puts imported, exempt
-  products = Product.new(qty, product, price, exempt, imported, 0)  
-  PRODUCTS_ARR.push(products)
-
-  no-=1 
-end
-
-new_receipt = Receipt.new
-
-PRODUCTS_ARR.each do |prod|
-  new_receipt.add_to_receipt(prod)
-end
-
-puts new_receipt.print_list
